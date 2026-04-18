33204f8b4f08abf6bdd19d17498126ba6f7150d3
Noosonic <68994083+Noosonic@users.noreply.github.com>
2026-04-18 12:16:58 +0700

    Initialize DisasterResponseStrategy interface & EarthquakeResponse class

---
+++ src/main/java/strategy/DisasterResponseStrategy.java
@@ -0,0 +1,5 @@
+package main.java.strategy;
+
+public interface DisasterResponseStrategy {
+    void transmitSafetyProcedures();
+}
---
+++ src/main/java/strategy/EarthquakeResponse.java
@@ -0,0 +1,10 @@
+package main.java.strategy;
+
+public class EarthquakeResponse implements DisasterResponseStrategy {
+
+
+    @Override
+    public void transmitSafetyProcedures() {
+        System.out.println("Earthquake Detected!");
+    }
+}
